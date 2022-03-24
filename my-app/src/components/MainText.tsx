b5e2634b60436ff33d6517a3697a771c372f13fb
ravgusha <ravgusha@gmail.com>
2022-03-24 15:28:51 +0300

    feat: add main text component

--- my-app/src/components/MainText.tsx
+++ my-app/src/components/MainText.tsx
@@ -6,11 +6,8 @@ class MainText extends Component {
   render() {
     return (
       <div className='container__text'>
-        <h1 className='container__title'>Looking For A Great Film To Watch This Evening At Home?</h1>
-        <p className='container__description'>
-          For all those movie therapy lovers, we have gathered the best of the best in every genre,
-          so that you could enjoy the iconic films once more or maybe even for the very first
-          time.Here we go!
+        <h1 className='container__title'>Find Your Perfect Movie</h1>
+        <p className='container__description'>For all those movie therapy lovers, we have gathered the best of the best in every genre.<br />Here we go!
         </p>
       </div>
     );
