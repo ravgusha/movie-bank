55af29383e670c3e78b06eca582905c98280401d
ravgusha <ravgusha@gmail.com>
2022-06-27 17:46:43 +0300

    feat: add createReducer

--- src/reducers/cardReducer.tsx
+++ src/reducers/cardReducer.tsx
@@ -1,4 +1,6 @@
 import { ApiCard } from '../components/CardList/CardList';
+import { addCardAction } from '../actions/cardActions';
+import { createReducer } from '@reduxjs/toolkit';
 
 export interface ICardState {
   cards: Array<ApiCard> | [];
@@ -10,16 +12,24 @@ const defaultState = {
   cards: [],
 };
 
-const cardReducer = (state: ICardState = defaultState, action: ActionType) => {
-  switch (action.type) {
-    case 'ADD_CARD':
-      return {
-        ...state,
-        cards: [...state.cards, action.payload]
-      };
-    default:
-      return state;
-  }
-};
+const cardReducer = createReducer(defaultState, (builder) => {
+  builder
+    .addCase(addCardAction, (state, action) => {
+      state.cards.push(action.payload);
+    })
+    .addDefaultCase(() => {});
+});
+
+// const cardReducer = (state: ICardState = defaultState, action: ActionType) => {
+//   switch (action.type) {
+//     case 'ADD_CARD':
+//       return {
+//         ...state,
+//         cards: [...state.cards, action.payload]
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
 export default cardReducer;
